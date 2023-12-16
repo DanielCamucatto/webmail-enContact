8aaa81fe16e9534625ba7f9ddea71cac1c3bca3a
Daniel <daniel@taller.net.br>
2023-12-15 23:19:13 -0300

    feat: add search input for component

---
+++ src/components/SearchInput/styles.ts
@@ -0,0 +1,3 @@
+import tw from 'tailwind-styled-components'
+
+export const SearchInputBox = tw.input`relative m-0 block min-w-0 flex-auto rounded-lg border border-solid border-2 border-slate-500 bg-transparent bg-clip-padding px-3 py-4 text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-orange-900 focus:text-slate-900 focus:shadow-[inset_0_0_0_1px_rgb(202, 104, 59)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary`
